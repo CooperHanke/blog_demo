33bfdd3ab979558a6dc7839a42ceb5b9c802143e
Cooper Hanke <cooper.hanke@gmail.com>
2018-04-17 15:24:59 -0500

    now have a unified post - most of the body is present to remember how i want it to look

--- src/main/java/guru/cooperhanke/blog_demo/controllers/Post.java
+++ src/main/java/guru/cooperhanke/blog_demo/controllers/Post.java
@@ -1,4 +1,23 @@
 package guru.cooperhanke.blog_demo.controllers;
 
 public class Post {
+
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
--- src/main/java/guru/cooperhanke/blog_demo/controllers/PostController.java
+++ src/main/java/guru/cooperhanke/blog_demo/controllers/PostController.java
@@ -1,20 +1,34 @@
 package guru.cooperhanke.blog_demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String post() {
-        return "posts index page";
+    public String post(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post1 = new Post();
+        Post post2 = new Post();
+        post1.setTitle("First Test");
+        post2.setTitle("Second Test");
+        post1.setBody("From arraylist");
+        post2.setBody("From arraylist");
+        model.addAttribute("post1", post1);
+        model.addAttribute("post2", post2);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String singlePost(@PathVariable int id) {
-        return "view the individual post for " + id;
+    public String singlePost(@PathVariable int id, Model model) {
+        Post post = new Post();
+        post.setTitle("Test Post " + id);
+        post.setBody("Body of test post " + id);
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
