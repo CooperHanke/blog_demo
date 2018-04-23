992d66dc66ec93aefd4a0051ffdec62c0cdaed2a
Cooper Hanke <cooper.hanke@gmail.com>
2018-04-23 14:39:33 -0500

    init commit of production code

--- src/main/java/guru/cooperhanke/blog_demo/BlogDemoApplication.java
+++ src/main/java/guru/cooperhanke/blog_demo/BlogDemoApplication.java
@@ -2,11 +2,17 @@ package guru.cooperhanke.blog_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogDemoApplication {
+public class BlogDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogDemoApplication.class, args);
+    }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+        return app.sources(BlogDemoApplication.class);
     }
 }
